5c45ee589560b5b75dd314f6eaf8bb2bdad72e09
Marcel Ribeiro Dantas <mribeirodantas@fedoraproject.org>
2015-07-08 23:41:29 -0300

    Added Page to admin interface
    - Set search_fields
    - Set list_display
    - Set list_filters

--- avexado/apps/wiki/admin.py
+++ avexado/apps/wiki/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from avexado.apps.wiki.models import Page
 
-# Register your models here.
+
+class PageAdmin(admin.ModelAdmin):
+    list_display = ('name', 'last_edited_at', )
+    list_filter = ['last_edited_at', ]
+    search_fields = ['content',]
+
+admin.site.register(Page, PageAdmin)
--- avexado/apps/wiki/models.py
+++ avexado/apps/wiki/models.py
@@ -5,3 +5,6 @@ class Page(models.Model):
     name = models.CharField(max_length='20', primary_key=True)
     content = models.TextField(blank=True, null=True)
     last_edited_at = models.DateTimeField(auto_now=True)
+
+    def __unicode__(self):
+        return self.name
